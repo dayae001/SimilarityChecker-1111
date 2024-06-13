ac201dac2cb2c6417c4a4a61cc1d1325d7500617
dayae001 <dayae001@naver.com>
2024-06-13 16:04:37 +0900

    [refactoring] A / BB

--- similarity_checker.py
+++ similarity_checker.py
@@ -5,17 +5,27 @@ class SimilarityChecker:
 
     def guess(self):
         score = 0
+        score += self.calc_length()
+        score += self.calc_alpha()
+        return score
+
+    def calc_length(self):
+        score = 0
+        
         if len(self.alpha1) == len(self.alpha2):
             score += 60
-
         elif len(self.alpha1) <= len(self.alpha2) * 0.5 or \
                 len(self.alpha1) >= len(self.alpha2) * 2:
             score += 0
+        return score
 
-        if set(self.alpha1) == set(self.alpha2):
+    def calc_alpha(self):
+        score = 0
+        alpha1_set = set(self.alpha1)
+        alpha2_set = set(self.alpha2)
+
+        if alpha1_set == alpha2_set:
             score += 40
-
-        elif len(set(self.alpha1).intersection(set(self.alpha2))) == 0:
+        elif len(alpha1_set.intersection(alpha2_set)) == 0:
             score += 0
-
         return score
